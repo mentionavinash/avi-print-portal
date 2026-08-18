4aff53f40040fa31c8fdbc3aa09a8319f9d77518
Avinash Kumar <aviinformatics@gmail.com>
2026-08-18 19:44:09 +0530

    Update Supabase publishable key in config.js

--- config.js
+++ config.js
@@ -1,8 +1,15 @@
+// ================================
+// AviPrint - Supabase Configuration
+// ================================
+
 const SUPABASE_URL =
     "https://pmqmewceqnxsvlhceeef.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
-    "APNI_NEW_PUBLISHABLE_KEY_YAHAN_PASTE_KARO";
+    "sb_publishable_mWPszaFzSHbPp-OHlPGWjw_JdrMU6oQ";
+
+
+// Create Supabase client
 
 const supabaseClient =
     window.supabase.createClient(
