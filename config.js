4b99288fce87f2fd32c30fa627b4697beaa30d33
Avinash Kumar <aviinformatics@gmail.com>
2026-08-18 19:50:13 +0530

    Clean up config.js by removing comments
    Removed comments and unnecessary whitespace in config.js.

--- config.js
+++ config.js
@@ -1,15 +1,8 @@
-// ================================
-// AviPrint - Supabase Configuration
-// ================================
-
 const SUPABASE_URL =
     "https://pmqmewceqnxsvlhceeef.supabase.co";
 
 const SUPABASE_PUBLISHABLE_KEY =
     "sb_publishable_mWPszaFzSHbPp-OHlPGWjw_JdrMU6oQ";
-
-
-// Create Supabase client
 
 const supabaseClient =
     window.supabase.createClient(
